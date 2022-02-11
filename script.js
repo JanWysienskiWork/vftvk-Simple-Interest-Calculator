18d03c25639377278222f033af0f2a0a6fbeb4a4
JanWysienskiWork <jan.wysienski@gmail.com>
2022-02-11 18:55:52 +0000

    Modifications made as instructed

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+/**Computing interest, function takes amount (principal), interest rate (rate)
+ * and year from local time then produces an html output */
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <span class=\"highlighted\">"+principal+"</span>,\<br\>at an interest rate of <span class=\"highlighted\">"+rate+"%\</span><br\>You will receive an amount of <span class=\"highlighted\">"+interest+"</span>,\<br\>in the year <span class=\"highlighted\">"+year+"\</span><br\>"
 }
+
+//Updating value next to the slider
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
+
         
